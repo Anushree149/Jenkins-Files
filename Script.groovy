3a6a88aba4cc9a0e1d825a301a3e8a6b7824bf78
Anushree <idekaranushree@gmail.com>
2024-09-20 18:15:43 +0530

    Added Deploy stage

--- Script.groovy
+++ Script.groovy
@@ -7,5 +7,10 @@ pipeline {
               git branch: "main", url:'https://github.com/Anushree149/JournalEntry.git' 
             } 
         } 
+        stage('Deploy') { 
+           steps {
+              sh "sudo mv . /var/www/html/" 
+            } 
+        } 
     } 
 }
