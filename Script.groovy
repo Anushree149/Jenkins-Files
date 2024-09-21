5511126726a657ec828e37b0a8ddc52625c41b28
Anushree <idekaranushree@gmail.com>
2024-09-21 16:59:27 +0530

    testing stage

--- Script.groovy
+++ Script.groovy
@@ -17,5 +17,44 @@ pipeline {
                 sh "sudo systemctl restart apache2"
             }
         }
+        post {
+        success {
+             emailext body: '''<html><body><h2>LMS PROD - Version # $BUILD_NUMBER - Deployment $BUILD_STATUS.</h2><br/>
+                        <br/>
+                        <h2>Deployed on lms.actifyzone.com</h2>
+                        <br/>
+                       <h3><b> Check console <a href="$BUILD_URL">output</a> to view full results.</b></h3><br/>
+                        <b><i>If you cannot connect to the build server, check the attached logs.</i></b><br/>
+                        <br/>
+                        --<br/>
+                        Following is the last 50 lines of the log.<br/>
+                        <br/>
+                        <b>--LOG-BEGIN--</b><br/>
+                        <pre style='line-height: 22px; display: block; color: #333; font-family: Monaco,Menlo,Consolas,"Courier New",monospace; padding: 10.5px; margin: 0 0 11px; font-size: 13px; word-break: break-all; word-wrap: break-word; white-space: pre-wrap; background-color: #f5f5f5; border: 1px solid #ccc; border: 1px solid rgba(0,0,0,.15); -webkit-border-radius: 4px; -moz-border-radius: 4px; border-radius: 4px;'>
+                        ${BUILD_LOG, maxLines=50, escapeHtml=true}
+                        </pre>
+                        <b>--LOG-END--</b></body></html>''',
+             subject:'LMS PROD Deployed Successfully - $PROJECT_NAME',
+             to: 'abdallahq989@gmail.com , abdallah.kammruddin@dextero.in , ashish.singh@dextero.in',
+             mimeType: 'text/html'
+        }
+        failure {
+            emailext body: '''<html><body><h2>LMS PROD - Build # $BUILD_NUMBER - Deployment $BUILD_STATUS.</h2><br/>
+                        <br/>
+                       <b> Check console <a href="$BUILD_URL">output</a> to view full results.</b><br/>
+                        <b><i>If you cannot connect to the build server, check the attached logs.</i></b><br/>
+                        <br/>
+                        --<br/>
+                        Following is the last 50 lines of the log.<br/>
+                        <br/>
+                        <b>--LOG-BEGIN--</b><br/>
+                        <pre style='line-height: 22px; display: block; color: #333; font-family: Monaco,Menlo,Consolas,"Courier New",monospace; padding: 10.5px; margin: 0 0 11px; font-size: 13px; word-break: break-all; word-wrap: break-word; white-space: pre-wrap; background-color: #f5f5f5; border: 1px solid #ccc; border: 1px solid rgba(0,0,0,.15); -webkit-border-radius: 4px; -moz-border-radius: 4px; border-radius: 4px;'>
+                        ${BUILD_LOG, maxLines=50, escapeHtml=true}
+                        </pre>
+                        <b>--LOG-END--</b></body></html>''',
+                     subject:'LMS Deployment failed :$PROJECT_NAME' , 
+                     to: 'abdallahq989@gmail.com , ashish.singh@dextero.in',
+                     mimeType: 'text/html'
+        }
     } 
 }
